6f6391ad053061a88608e0aeca976e216b5a228b
Eric Geniesse <egeniesse@gmail.com>
2016-02-16 08:52:43 -0800

    First commit

---
+++ server/server.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path')
+var port = process.env.PORT || 3000;
+var fs = require("fs");
+var officegen = require('officegen');
+var docx = officegen ( 'docx' );
+
+
+
+app.use(express.static(__dirname + '/../templateGenerator'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.post('*', function(req, res){
+	var content = req.body.data;
+	docx.on ( 'finalize', function ( written ) {
+				console.log ( 'Finish to create Word file.\nTotal bytes created: ' + written + '\n' );
+			});
+
+	docx.on ( 'error', function ( err ) {
+				console.log ( err );
+			});
+
+	var pObj = docx.createP ();
+	pObj.addText (content.name, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addText (content.city, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addText (content.phoneNumber, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addText (content.email, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.date, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.intro, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.paragraphOne, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.paragraphTwo, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.paragraphThree, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.regards, { font_face: 'Helvetica Neue' });
+	pObj.addLineBreak ();
+	pObj.addLineBreak ();
+	pObj.addText (content.name, { font_face: 'Helvetica Neue' });
+	docx.putPageBreak ();
+	console.log(req.body.companyName)
+	var out = fs.createWriteStream (path.join(__dirname + '/../../../coverLetters/' + req.body.companyName + '.docx'));
+
+	out.on('error',function(err){
+		console.log(err);
+	});
+
+	docx.generate(out);
+	res.sendStatus(200)
+});
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname + '/../templateGenerator/cover.html'));
+
+});app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
---
+++ templateGenerator/coverModule.js
@@ -0,0 +1,55 @@
+angular.module('coverTemplate', ['ngMaterial'])
+
+  .controller('updateForm', function($scope, $mdDialog, $http){
+    var currentDay = new Date();
+    var date = currentDay.getMonth() + "/" + currentDay.getDate() + "/" + currentDay.getFullYear();
+    $scope.date = date;
+    $scope.company = "Company";
+    $scope.stack = "full stack"
+    $scope.firstFramework = 'insert first framework here';
+    $scope.firstAppDescription = 'describe what exactly it did that was so cool';
+    $scope.descriptionOfWorkOnAppOne = 'description of work on first app';
+    $scope.descriptionOfWorkOnAppTwo = 'description of work on second app';
+    $scope.secondAppDescription = 'describe what exactly it did that was so cool';
+    $scope.secondApp = 'applicable technology';
+    $scope.applicableFramework = 'insert applicable framework here';
+    $scope.reasonToWork = 'help improve countless lives of potential users by growing your company and making it better'
+    $scope.submitCoverLetter = function(){
+      var name = $('#name').text();
+      var city = $('#city').text();
+      var phoneNumber = $('#phoneNumber').text();
+      var email = $('#email').text();
+      var date = $('#date').text();
+      var intro = $('#intro').text();
+      var paragraphOne = $('#paragraphOne').text();
+      var paragraphTwo = $('#paragraphTwo').text();
+      var paragraphThree = $('#paragraphThree').text();
+      var regards = $('#regards').text();
+      var req = {
+       method: 'POST',
+       url: 'http://localhost:3000/',
+       headers: {
+         'Content-Type': 'application/json'
+       },
+       data: { 
+        data : {
+          name: name,
+          city: city,
+          phoneNumber: phoneNumber,
+          email: email,
+          date: date,
+          intro: intro,
+          paragraphOne: paragraphOne,
+          paragraphTwo: paragraphTwo,
+          paragraphThree: paragraphThree,
+          regards: regards
+        },
+        companyName : $scope.company
+        }
+      }
+      $http(req)
+        .then(function(err, res){
+        })
+    }
+
+  })
